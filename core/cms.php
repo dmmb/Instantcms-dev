e19a13f92ce627393660ce756f163ada6262575a
r2 <r2@0bae1178-1a42-4f3f-84ab-940c0a723382>
2010-11-16 08:54:18 +0000

    smarty lazy load

--- core/cms.php
+++ core/cms.php
@@ -38,6 +38,8 @@ class cmsCore {
     private         $module_configs = array();
     private         $component_configs = array();
 
+    private         $smarty = false;
+
     private function __construct($install_mode=false) {
 
         if ($install_mode){ return; }
@@ -1805,59 +1807,60 @@ class cmsCore {
      */
     public function loadSmarty(){
         $this->includeFile('/includes/smarty/libs/Smarty.class.php');
+        $this->smarty = new Smarty();
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /**
      * Возвращает объект Smarty для дальнейшей работы с шаблоном
-     * @param string $for = modules / components / plugins
-     * @param string $tpl
+     * @param string $tpl_folder = modules / components / plugins
+     * @param string $tpl_file
      * @return obj
      */
-    public function initSmarty($for='modules', $tpl=''){ //cmsSmartyInit
-        global $smarty;
-        global $_CFG;
+    public function initSmarty($tpl_folder='modules', $tpl_file=''){
+
         global $_LANG;
-        $smarty->compile_dir = PATH.'/cache';
 
-        if (!is_writable($smarty->compile_dir)){ @chmod($smarty->compile_dir, 0755); }
+        if (!$this->smarty){ $this->loadSmarty(); }
 
-        if(file_exists(PATH.'/templates/'.$_CFG['template'].'/'.$for.'/'.$tpl)){
-            $smarty->template_dir = PATH.'/templates/'.$_CFG['template'].'/'.$for;
-        } else {
-            $smarty->template_dir = PATH.'/templates/_default_/'.$for;
-        }
-        // Передаем языковый массив в шаблон
-        $smarty->assign('LANG', $_LANG);
-        $smarty->register_modifier("NoSpam", "cmsSmartyNoSpam");
-        $smarty->register_function('add_js', 'cmsSmartyAddJS');
-        $smarty->register_function('add_css', 'cmsSmartyAddCSS');
-        $smarty->register_function('wysiwyg', 'cmsSmartyWysiwyg');
-        $smarty->register_function('comments', 'cmsSmartyComments');
-        $smarty->register_function('profile_url', 'cmsSmartyProfileURL');
+        $this->smarty->compile_dir = PATH.'/cache';
 
-        return $smarty;
+        $template_has_tpl = file_exists(TEMPLATE_DIR . "{$tpl_folder}/{$tpl_file}");
+
+        $this->smarty->template_dir = $template_has_tpl ? TEMPLATE_DIR . $tpl_folder : DEFAULT_TEMPLATE_DIR . $tpl_folder;
+
+        $this->smarty->assign('LANG', $_LANG);
+        $this->smarty->register_modifier("NoSpam", "cmsSmartyNoSpam");
+        $this->smarty->register_function('add_js', 'cmsSmartyAddJS');
+        $this->smarty->register_function('add_css', 'cmsSmartyAddCSS');
+        $this->smarty->register_function('wysiwyg', 'cmsSmartyWysiwyg');
+        $this->smarty->register_function('comments', 'cmsSmartyComments');
+        $this->smarty->register_function('profile_url', 'cmsSmartyProfileURL');
+
+        return $this->smarty;
+        
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public function initSmartyModule(){ //cmsSmartyInitModule
-        global $smarty;
-        $smarty->compile_dir = PATH.'/cache';
-        if(@is_dir(TEMPLATE_DIR.'modules')){
-            $smarty->template_dir = TEMPLATE_DIR.'modules';
-        } else {
-            $smarty->template_dir = PATH.'/templates/_default_/modules';
-        }
+    public function initSmartyModule(){
 
-        $smarty->register_modifier("NoSpam", "cmsSmartyNoSpam");
-        $smarty->register_function('add_js', 'cmsSmartyAddJS');
-        $smarty->register_function('add_css', 'cmsSmartyAddCSS');
-        $smarty->register_function('wysiwyg', 'cmsSmartyWysiwyg');
-        $smarty->register_function('profile_url', 'cmsSmartyProfileURL');
+        if (!$this->smarty){ $this->loadSmarty(); }
 
-        return $smarty;
+        $this->smarty->compile_dir = PATH.'/cache';
+
+        $template_has_dir = is_dir(TEMPLATE_DIR.'modules');
+
+        $this->smarty->template_dir = $template_has_dir ? TEMPLATE_DIR.'modules' : DEFAULT_TEMPLATE_DIR.'modules';
+
+        $this->smarty->register_modifier("NoSpam", "cmsSmartyNoSpam");
+        $this->smarty->register_function('add_js', 'cmsSmartyAddJS');
+        $this->smarty->register_function('add_css', 'cmsSmartyAddCSS');
+        $this->smarty->register_function('wysiwyg', 'cmsSmartyWysiwyg');
+        $this->smarty->register_function('profile_url', 'cmsSmartyProfileURL');
+
+        return $this->smarty;
     }
 
     // CONFIGS //////////////////////////////////////////////////////////////////////////////////////////////////////////////
