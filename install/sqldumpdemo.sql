a6b493fa653b2ef75808f70ffa8767f4c67d16c6
r2 <r2@0bae1178-1a42-4f3f-84ab-940c0a723382>
2010-11-21 11:32:01 +0000

    убрал ругань про php5.3 в инсталлере

--- install/sqldumpdemo.sql
+++ install/sqldumpdemo.sql
@@ -493,13 +493,13 @@ CREATE TABLE `#__cron_jobs` (
   KEY `job_name` (`job_name`,`is_enabled`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=cp1251;
 
-INSERT INTO `#__cron_jobs` (`id`, `job_name`, `job_interval`, `job_run_date`, `component`, `model_method`, `custom_file`, `is_enabled`, `is_new`, `comment`, `class_name`, `class_method`) VALUES
-(1, 'photos_clear', 24, '2010-11-11 20:53:13', 'users', 'clearUploadedPhotos', '', 1, 1, 'Удаление неиспользуемых личных фотографий', '', ''),
-(2, 'optimize_tables', 24, '2010-11-14 14:52:45', '', '', '', 1, 1, 'Оптимизация таблиц БД', 'db|cmsDatabase', 'optimizeTables'),
-(3, 'drop_inactive_users', 48, '2010-11-14 14:58:24', 'users', 'deleteInactiveUsers', '', 1, 1, 'Удаление неактивных пользователей (см. настройки компонента "Профили пользователей")', '', ''),
-(4, 'remove_old_log', 48, '2010-11-14 14:58:35', '', '', '', 1, 1, 'Удаляет старые записи ленты событий', 'actions|cmsActions', 'removeOldLog'),
-(5, 'give_invites', 24, '2010-11-15 20:30:03', 'users', 'giveInvitesCron', '', 1, 1, 'Выдача инвайтов пользователям', '', ''),
-(6, 'clear_invites', 24, '2010-11-15 20:30:33', 'users', 'clearInvites', '', 1, 1, 'Удаление использованных инвайтов', '', '');
+INSERT INTO `#__cron_jobs` (`id`, `job_name`, `job_interval`, `component`, `model_method`, `custom_file`, `is_enabled`, `is_new`, `comment`, `class_name`, `class_method`) VALUES
+(1, 'photos_clear', 24, 'users', 'clearUploadedPhotos', '', 1, 1, 'Удаление неиспользуемых личных фотографий', '', ''),
+(2, 'optimize_tables', 24, '', '', '', 1, 1, 'Оптимизация таблиц БД', 'db|cmsDatabase', 'optimizeTables'),
+(3, 'drop_inactive_users', 48, 'users', 'deleteInactiveUsers', '', 1, 1, 'Удаление неактивных пользователей (см. настройки компонента "Профили пользователей")', '', ''),
+(4, 'remove_old_log', 48, '', '', '', 1, 1, 'Удаляет старые записи ленты событий', 'actions|cmsActions', 'removeOldLog'),
+(5, 'give_invites', 24, 'users', 'giveInvitesCron', '', 1, 1, 'Выдача инвайтов пользователям', '', ''),
+(6, 'clear_invites', 24, 'users', 'clearInvites', '', 1, 1, 'Удаление использованных инвайтов', '', '');
 
 DROP TABLE IF EXISTS `#__downloads`;
 CREATE TABLE `#__downloads` (
